e408c9139181686c78df8865f591d039d65b4a81
Michael DOHR <dohr.michael@gmail.com>
2017-10-12 07:24:18 +0200

    Updates

--- relationship-app/src/app/shared/services/api.service.ts
+++ relationship-app/src/app/shared/services/api.service.ts
@@ -6,36 +6,36 @@ import { environment } from '../../../environments/environment';
 @Injectable()
 export class ApiService {
   constructor( private http: Http ) {}
-  
+
   private getHeaders(): Headers {
     const headersConfig = {
       'Content-Type': 'application/json',
-      'Accept':       'application/json'
+      'Accept': 'application/json'
     };
     // TODO Bearer
     return new Headers( headersConfig );
   }
-  
+
   private formatErrors( error: any ) {return Observable.throw( error.json() );}
-  
+
   get<T = any>( path: string, params: URLSearchParams = new URLSearchParams() ): Observable<T> {
     return this.http.get( `${environment.api_url}${path}`, { headers: this.getHeaders(), search: params } )
       .catch( this.formatErrors )
       .switchMap( ( res: Response ) => Observable.fromPromise( res.json() ) );
   }
-  
+
   put<T = any>( path: string, body: Object = {} ): Observable<T> {
     return this.http.put( `${environment.api_url}${path}`, JSON.stringify( body ), { headers: this.getHeaders() } )
       .catch( this.formatErrors )
       .switchMap( ( res: Response ) => Observable.fromPromise( res.json() ) );
   }
-  
+
   post<T = any>( path: string, body: Object = {} ): Observable<T> {
     return this.http.post( `${environment.api_url}${path}`, JSON.stringify( body ), { headers: this.getHeaders() } )
       .catch( this.formatErrors )
       .switchMap( ( res: Response ) => Observable.fromPromise( res.json() ) );
   }
-  
+
   delete<T = any>( path ): Observable<T> {
     return this.http.delete( `${environment.api_url}${path}`, { headers: this.getHeaders() } )
       .catch( this.formatErrors )
